e981aac73c1398b73f198365a53c4ddf76cc26f0
ezawa800 <sho.ezawa.dev@gmail.com>
2018-05-30 11:11:54 +0900

    init vote page

---
+++ src/components/pages/vote.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  div(class="vote-page")
+    VoteButton
+</template>
+
+<script>
+import VoteButton from '@/components/atoms/VoteButton'
+
+export default {
+  name: 'vote',
+  components: {
+    VoteButton
+  }
+}
+</script>
+
+<style lang="stylus">
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/components/pages/index'
+import vote from '@/components/pages/vote'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'index',
       component: index
+    },
+    {
+      path: '/vote',
+      name: 'vote',
+      component: vote
     }
   ]
 })
