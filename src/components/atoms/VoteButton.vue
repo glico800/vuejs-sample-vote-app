91778239dabf1523469f37661db2cd08fe9193a7
ezawa800 <sho.ezawa.dev@gmail.com>
2018-05-30 11:11:42 +0900

    init VoteButton

---
+++ src/components/atoms/VoteButton.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  button(class="vote-button")
+    img(src="@/assets/logo.png")
+    span.name Vue.js
+    span.count {{ count }}
+</template>
+
+<script>
+export default {
+  name: 'VoteButton',
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+
+</style>
