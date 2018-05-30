79bfeeeedec391c23c6ac50efc0fce6855c65e05
ezawa800 <sho.ezawa.dev@gmail.com>
2018-05-30 21:11:35 +0900

    add winner image

--- src/components/organisms/VoteUnit.vue
+++ src/components/organisms/VoteUnit.vue
@@ -1,15 +1,17 @@
 <template lang="pug">
   div(class="vote-unit")
-    p {{counts.ember}}, {{counts.react}}, {{counts.riot}}, {{counts.vue}}
+    VoteImage(:targetName="winner")
     VoteButtonList(@countup-from-list="countupOf")
 </template>
 
 <script>
+import VoteImage from '@/components/atoms/VoteImage'
 import VoteButtonList from '@/components/molecules/VoteButtonList'
 
 export default {
   name: 'VoteUnit',
   components: {
+    VoteImage,
     VoteButtonList
   },
   data() {
@@ -19,12 +21,20 @@ export default {
         react: 0,
         riot: 0,
         vue: 0
-      }
+      },
+      winner: 'ember'
     }
   },
   methods: {
     countupOf: function(targetName) {
       this.counts[targetName]++
+      this.updateWinner()
+    },
+    updateWinner: function() {
+      const maxCount = Math.max.apply(null, Object.values(this.counts))
+      this.winner = Object.keys(this.counts).filter(key => {
+        return this.counts[key] === maxCount
+      })[0]
     }
   }
 }
