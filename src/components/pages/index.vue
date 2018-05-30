5fcf40b57efb695d59312b568cfce3e9f374bb01
ezawa800 <sho.ezawa.dev@gmail.com>
2018-05-30 10:51:51 +0900

    add index.vue

---
+++ src/components/pages/index.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  div(class="index")
+    HelloWorld
+</template>
+
+<script>
+import HelloWorld from '@/components/atoms/HelloWorld'
+
+export default {
+  name: 'index',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="stylus">
+</style>
