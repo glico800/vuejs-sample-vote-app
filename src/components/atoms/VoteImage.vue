3df50904dab8d5b7d1068e090bc7b3639a8cd293
ezawa800 <sho.ezawa.dev@gmail.com>
2018-05-30 15:40:11 +0900

    add other logo image

--- src/components/atoms/VoteImage.vue
+++ src/components/atoms/VoteImage.vue
@@ -8,18 +8,27 @@ export default {
   props: {
     targetName: {
       validator: function(value) {
-        return ['vue'].includes(value)
+        return ['ember', 'react', 'riot', 'vue'].includes(value)
       }
     }
   },
   data() {
     return {
+      ember_image: require('@/assets/ember_logo.png'),
+      react_image: require('@/assets/react_logo.png'),
+      riot_image: require('@/assets/riot_logo.png'),
       vue_image: require('@/assets/vue_logo.png')
     }
   },
   computed: {
     image_src: function() {
       switch (this.targetName) {
+        case 'ember':
+          return this.ember_image
+        case 'react':
+          return this.react_image
+        case 'riot':
+          return this.riot_image
         case 'vue':
           return this.vue_image
       }
